42750cc177f05cd44af598d0b361b365d7c03154
Gauri <gauri@examle.com>
2019-03-19 11:43:17 -0700

    jQuery slider assignment 2

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,7 +1,7 @@
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
-    $('#time-control .total-time').text( player.getDuration() );
+    $('#time-control .total-time').text(player.currentlyPlaying.duration );
   }
 }
 const helper = new Helper();
